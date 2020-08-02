f3fdd6ebb3b3a99e92fdafd470f5c50515906d4a
bombody <135224248+bombody@users.noreply.github.com>
2020-08-02 18:52:31 +0000

    Move other-requirements.txt to bindep.txt

--- examples/video_probe.py
+++ examples/video_probe.py
@@ -17,4 +17,14 @@ def gen_system_time_request():
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Chrysalis Edge buffered images example')
- 
+    parser.add_argument("--device", type=str, default=None, required=True)
+    args = parser.parse_args()
+    device_id = args.device
+    
+    channel = grpc.insecure_channel('127.0.0.1:50001')
+    stub = video_streaming_pb2_grpc.ImageStub(channel)
+
+    probe = stub.VideoProbe(gen_buffer_probe_request(device_name=device_id))
+    print(probe)
+
+    sysTime = stub.Sys
