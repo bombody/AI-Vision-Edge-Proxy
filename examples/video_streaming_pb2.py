14facaef9de1f5d4ecdeb044281db06ada8828b8
bombody <135224248+bombody@users.noreply.github.com>
2020-09-11 20:29:11 +0000

    Spec

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -41,4 +41,10 @@ _ANNOTATEREQUEST = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='remote_stream_id', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateReque
+      name='remote_stream_id', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateRequest.remote_stream_id', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descrip
