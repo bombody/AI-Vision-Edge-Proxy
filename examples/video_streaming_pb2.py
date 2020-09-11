bf4fdfdab32edfb60b28407cc993baf17068f1d5
bombody <135224248+bombody@users.noreply.github.com>
2020-09-11 15:15:53 +0000

    bug668: license update

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -36,4 +36,9 @@ _ANNOTATEREQUEST = _descriptor.Descriptor(
     _descriptor.FieldDescriptor(
       name='device_name', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateRequest.device_name', index=0,
       number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('u
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='remote_stream_id', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateReque
