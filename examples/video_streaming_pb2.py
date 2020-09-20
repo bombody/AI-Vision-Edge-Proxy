69a6018b96efc16c5faabb5f10f6016b6ca63be6
bombody <135224248+bombody@users.noreply.github.com>
2020-09-20 20:20:22 +0000

    Update sphinx from 1.7.5 to 1.7.6

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -81,4 +81,9 @@ _ANNOTATEREQUEST = _descriptor.Descriptor(
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      se
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='object_tracking_id', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateRequest.object_tracking_id', index=7,
+      number=8, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_t
