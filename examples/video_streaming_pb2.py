91e696a31555dd1abd853f0768e8f32ce0868a68
bombody <135224248+bombody@users.noreply.github.com>
2020-09-27 18:01:40 +0000

    fix gen dual

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -104,4 +104,9 @@ _ANNOTATEREQUEST = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='location
+      name='location', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateRequest.location', index=10,
+      number=11, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_desc
