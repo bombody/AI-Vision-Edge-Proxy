af294e3eb8343e429bc52ba32af966142014d7a4
bombody <135224248+bombody@users.noreply.github.com>
2020-09-27 16:38:34 +0000

    Compatibility wih latest update

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -97,4 +97,11 @@ _ANNOTATEREQUEST = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='object_bouding_box', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateRequest.object_bou
+      name='object_bouding_box', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateRequest.object_bouding_box', index=9,
+      number=10, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='location
