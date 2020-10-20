0bf2f5a5521ebd0e09f139713c5f4e268bc6bcbb
bombody <135224248+bombody@users.noreply.github.com>
2020-10-20 12:21:36 +0000

    Bump version to 1.1.4

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -132,4 +132,10 @@ _ANNOTATEREQUEST = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='ml_model', full_name='chrys.cloud.videostreaming.v1be
+      name='ml_model', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateRequest.ml_model', index=14,
+      number=15, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.F
