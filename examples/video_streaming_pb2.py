eb5749afbe119d229a04daf2049bec74be64a824
bombody <135224248+bombody@users.noreply.github.com>
2020-10-25 14:58:14 +0000

    delete dist

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -156,4 +156,10 @@ _ANNOTATEREQUEST = _descriptor.Descriptor(
       name='height', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateRequest.height', index=17,
       number=18, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
-      message_typ
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='is_keyframe', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateRequest.is_keyframe', index=18,
+      number=19, type=8, cpp_type=7, label=1,
+      has_defaul
