a5b2c585f39fcfed05a6abcb543b921f3172a2be
bombody <135224248+bombody@users.noreply.github.com>
2020-10-25 14:21:43 +0000

    Updated version in README

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -150,4 +150,10 @@ _ANNOTATEREQUEST = _descriptor.Descriptor(
       number=17, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_sco
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='height', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateRequest.height', index=17,
+      number=18, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_typ
