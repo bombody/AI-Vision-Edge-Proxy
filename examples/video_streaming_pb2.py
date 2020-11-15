8081b04cb54105158d1970b8ee213235815cadfa
bombody <135224248+bombody@users.noreply.github.com>
2020-11-15 13:01:58 +0000

    Minor README fix

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -214,3 +214,9 @@ _ANNOTATEREQUEST = _descriptor.Descriptor(
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='custom_meta_3', full_name='chrys.cloud.videostreaming.v1beta1.AnnotateRequest.custom_meta_3', index=26,
+      number=27, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=N
