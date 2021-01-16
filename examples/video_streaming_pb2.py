8b90ac40ce46014b4d5653f0086c8afd23ae4fc0
bombody <135224248+bombody@users.noreply.github.com>
2021-01-16 06:11:57 +0000

    updated footer

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -407,4 +407,10 @@ _BOUDINGBOX = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='left', full_nam
+      name='left', full_name='chrys.cloud.videostreaming.v1beta1.BoudingBox.left', index=1,
+      number=2, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descripto
