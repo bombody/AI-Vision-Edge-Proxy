7975d0dc7a4fac137b032ce78d220c4a529854f0
bombody <135224248+bombody@users.noreply.github.com>
2021-01-17 09:26:44 +0000

    updated commands

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -419,4 +419,11 @@ _BOUDINGBOX = _descriptor.Descriptor(
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='height', full_name='chrys.cloud.videostreaming.v1beta1.BoudingBox.height', index=3,
+      number=4, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPT
