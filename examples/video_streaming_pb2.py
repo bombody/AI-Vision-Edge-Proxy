81e1d5d693848452b4b8ce9db0f042845c5d5172
bombody <135224248+bombody@users.noreply.github.com>
2021-02-16 05:33:40 +0000

    cifs: on CONFIG_HIGHMEM machines, limit the rsize/wsize to the kmap space

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -542,4 +542,11 @@ _VIDEOFRAME = _descriptor.Descriptor(
       has_default_value=False, default_value=b"",
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._in
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='timestamp', full_name='chrys.cloud.videostreaming.v1beta1.VideoFrame.timestamp', index=3,
+      number=4, type=3, cpp_type=2, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serializ
