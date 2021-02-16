3a438435f88820d23428df73e6a0c5209aaa9a3e
bombody <135224248+bombody@users.noreply.github.com>
2021-02-16 14:21:27 +0000

    Conf

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -572,4 +572,9 @@ _VIDEOFRAME = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='frame_type', full_name='chrys.cloud
+      name='frame_type', full_name='chrys.cloud.videostreaming.v1beta1.VideoFrame.frame_type', index=7,
+      number=8, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._inte
