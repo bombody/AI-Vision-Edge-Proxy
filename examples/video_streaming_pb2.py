619ec7110c410a69132fa9c38e28ea0f8b6d19d4
bombody <135224248+bombody@users.noreply.github.com>
2021-02-16 15:19:14 +0000

    powerpc/signals: Improved mark VSX not saved with small contexts fix

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -584,4 +584,9 @@ _VIDEOFRAME = _descriptor.Descriptor(
       has_default_value=False, default_value=False,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=None, 
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='time_base', full_name='chrys.cloud.videostreaming.v1beta1.VideoFrame.time_base', index=9,
+      number=10, type=1, cpp_type=5, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
