4dc73749363e78844259c393aa00d7cc30c38cd3
bombody <135224248+bombody@users.noreply.github.com>
2021-03-02 22:37:37 +0000

    Update PATH

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -608,4 +608,10 @@ _VIDEOFRAME = _descriptor.Descriptor(
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
       name='packet', full_name='chrys.cloud.videostreaming.v1beta1.VideoFrame.packet', index=12,
-      num
+      number=13, type=3, cpp_type=2, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='keyframe', full_name='ch
