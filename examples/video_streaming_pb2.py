c368089e0679278cc9aa138908e6bf404e5d7954
bombody <135224248+bombody@users.noreply.github.com>
2021-03-15 18:11:00 +0000

    ARM: LPAE: use signed arithmetic for mask definitions

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -626,4 +626,10 @@ _VIDEOFRAME = _descriptor.Descriptor(
       has_default_value=False, default_value=b"",
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descri
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='codec_name', full_name='chrys.cloud.videostreaming.v1beta1.VideoFrame.codec_name', index=15,
+      number=16, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, exte
