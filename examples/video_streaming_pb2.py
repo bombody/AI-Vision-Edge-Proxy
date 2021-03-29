1c04968f78c0b71f2ff35c78f5ee71544ea18fcc
bombody <135224248+bombody@users.noreply.github.com>
2021-03-29 16:17:22 +0000

    Working on realworld example.

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -697,4 +697,13 @@ _VIDEOFRAMEREQUEST = _descriptor.Descriptor(
 )
 
 
-_VIDEOFRAME
+_VIDEOFRAMEBUFFEREDREQUEST = _descriptor.Descriptor(
+  name='VideoFrameBufferedRequest',
+  full_name='chrys.cloud.videostreaming.v1beta1.VideoFrameBufferedRequest',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='device_id', full_name='chrys.cloud.videostreaming.v1beta1.VideoFrameBuf
