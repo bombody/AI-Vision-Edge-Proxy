172abf0e33ebbc42e49d7d698531d20397eed342
bombody <135224248+bombody@users.noreply.github.com>
2021-04-11 20:07:24 +0000

    tracing: Protect tracer flags with trace_types_lock

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -718,4 +718,10 @@ _VIDEOFRAMEBUFFEREDREQUEST = _descriptor.Descriptor(
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=N
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='timestamp_to', full_name='chrys.cloud.videostreaming.v1beta1.VideoFrameBufferedRequest.timestamp_to', index=2,
+      number=3, type=3, cpp_type=2, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+    
