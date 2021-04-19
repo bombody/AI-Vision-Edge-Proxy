b818b6151809b1ed7fe2892f14abb3791b7eca56
bombody <135224248+bombody@users.noreply.github.com>
2021-04-19 16:43:11 +0000

    Update to HEAD of submodules

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -724,4 +724,19 @@ _VIDEOFRAMEBUFFEREDREQUEST = _descriptor.Descriptor(
       number=3, type=3, cpp_type=2, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
-    
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=1712,
+  serialized_end=1
