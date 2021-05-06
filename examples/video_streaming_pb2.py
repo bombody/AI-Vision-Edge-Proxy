b0222761e9f6c76cdfa63d1716bb75db52d3c7a2
bombody <135224248+bombody@users.noreply.github.com>
2021-05-06 10:07:03 +0000

    zz

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -764,4 +764,10 @@ _LISTSTREAM = _descriptor.Descriptor(
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_de
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='failing_streak', full_name='chrys.cloud.videostreaming.v1beta1.ListStream.failing_streak', index=2,
+      number=3, type=3, cpp_type=2, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension
