10629a0d41853f9b149402ba8808f5748d12d6ea
bombody <135224248+bombody@users.noreply.github.com>
2021-05-07 12:01:15 +0000

    Fixed several bugs

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -776,4 +776,10 @@ _LISTSTREAM = _descriptor.Descriptor(
       name='health_status', full_name='chrys.cloud.videostreaming.v1beta1.ListStream.health_status', index=3,
       number=4, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=b"".decode('utf-8'),
-  
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='dead', full_name='chrys.cloud.videostreaming.v1beta1.ListStream.dead', index=4,
+      number=5, type=8, cpp_type=7, label=1,
+      has_default_
