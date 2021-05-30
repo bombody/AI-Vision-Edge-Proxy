98be097aab9d5d35cdd32ba48b621f7c02ded1de
bombody <135224248+bombody@users.noreply.github.com>
2021-05-30 16:27:20 +0000

    Release 1.4.0

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -812,4 +812,10 @@ _LISTSTREAM = _descriptor.Descriptor(
       number=9, type=8, cpp_type=7, label=1,
       has_default_value=False, default_value=False,
       message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=N
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='restarting', full_name='chrys.cloud.videostreaming.v1beta1.ListStream.restarting', index=9,
+      number=10, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      me
