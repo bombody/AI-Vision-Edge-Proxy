8766b06cebc783015af9847b915e412a049e8ed8
bombody <135224248+bombody@users.noreply.github.com>
2021-06-16 13:54:21 +0000

    Add article

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -893,4 +893,15 @@ _PROXYREQUEST = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='passthrough', full_name='chrys.cloud.videostreaming.v1beta1.ProxyRequest.pas
+      name='passthrough', full_name='chrys.cloud.videostreaming.v1beta1.ProxyRequest.passthrough', index=1,
+      number=2, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ]
