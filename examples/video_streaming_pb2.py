bfd657ee57d19a3bb7bd7ca9e717b83769c739dd
bombody <135224248+bombody@users.noreply.github.com>
2021-06-29 08:06:59 +0000

    Remove unnecessary comment.

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -950,4 +950,18 @@ _PROXYRESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_st
+  serialized_start=2108,
+  serialized_end=2163,
+)
+
+
+_STORAGEREQUEST = _descriptor.Descriptor(
+  name='StorageRequest',
+  full_name='chrys.cloud.videostreaming.v1beta1.StorageRequest',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='device_id', full_name='chrys.cloud.v
