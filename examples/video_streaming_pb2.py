97de1ddc42e98ec2dc3d464231723954c470929c
bombody <135224248+bombody@users.noreply.github.com>
2021-06-29 09:52:25 +0000

    move packages

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -964,4 +964,10 @@ _STORAGEREQUEST = _descriptor.Descriptor(
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='device_id', full_name='chrys.cloud.v
+      name='device_id', full_name='chrys.cloud.videostreaming.v1beta1.StorageRequest.device_id', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.F
