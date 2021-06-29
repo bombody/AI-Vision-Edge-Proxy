e9c3042adb639648aa45695751ee9c6bca110025
bombody <135224248+bombody@users.noreply.github.com>
2021-06-29 20:38:59 +0000

    Change theme

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -1010,4 +1010,16 @@ _STORAGERESPONSE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='start', full_name='chrys.cloud.videostreaming.v1beta1.StorageResponse.start',
+      name='start', full_name='chrys.cloud.videostreaming.v1beta1.StorageResponse.start', index=1,
+      number=2, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  seriali
