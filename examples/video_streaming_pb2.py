9566a8409d429346d78dd7d30f93e721b6dd0550
bombody <135224248+bombody@users.noreply.github.com>
2021-07-06 11:19:22 +0000

    bump version to 0.6.2

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -1039,4 +1039,11 @@ _VIDEOCODEC = _descriptor.Descriptor(
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
-  create_key=_descriptor._intern
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='name', full_name='chrys.cloud.videostreaming.v1beta1.VideoCodec.name', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_ext
