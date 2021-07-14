d0dd6bb001a23a54d037c5ac153fcb188d387fce
bombody <135224248+bombody@users.noreply.github.com>
2021-07-14 05:39:49 +0000

    jvm

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -1057,4 +1057,10 @@ _VIDEOCODEC = _descriptor.Descriptor(
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
       name='height', full_name='chrys.cloud.videostreaming.v1beta1.VideoCodec.height', index=2,
-      number=3, type=5, cpp_type=1, labe
+      number=3, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='pix_fmt', full_name='chrys.cloud.videostreaming.v1beta1.VideoCodec.pix_fmt', in
