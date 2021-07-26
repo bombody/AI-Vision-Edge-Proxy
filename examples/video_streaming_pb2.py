a5d28b60f8f0cb64841e503238fca3283dfe22ab
bombody <135224248+bombody@users.noreply.github.com>
2021-07-26 17:08:29 +0000

    Roll Chrome Win32 PGO Profile

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -1153,4 +1153,10 @@ _VIDEOPROBERESPONSE = _descriptor.Descriptor(
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=None, file=D
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='buffer', full_name='chrys.cloud.videostreaming.v1beta1.VideoProbeResponse.buffer', index=1,
+      number=2, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=F
