955f7ca3994906522a8c1b2f1731db0e0a579df9
bombody <135224248+bombody@users.noreply.github.com>
2021-08-02 11:12:02 +0000

    Release 7.0.1

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -1206,4 +1206,11 @@ _VIDEOBUFFER = _descriptor.Descriptor(
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  cr
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='approximate_fps', full_name='chrys.cloud.videostreaming.v1beta1.VideoBuffer.approximate_fps', index=3,
+      number=4, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      s
