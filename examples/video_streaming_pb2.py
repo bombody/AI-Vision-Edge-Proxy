1bfee33f6f24d2f0ef105cb500066d07cbe9c564
bombody <135224248+bombody@users.noreply.github.com>
2021-08-12 18:24:44 +0000

    Setting version to 0.3.0

--- examples/video_streaming_pb2.py
+++ examples/video_streaming_pb2.py
@@ -1247,4 +1247,13 @@ _SYSTEMTIMERESPONSE = _descriptor.Descriptor(
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='current_time', full_name='chrys.cloud.videostreaming.v1beta1.SystemTimeResponse.current_time', 
+      name='current_time', full_name='chrys.cloud.videostreaming.v1beta1.SystemTimeResponse.current_time', index=0,
+      number=1, type=3, cpp_type=2, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  neste
