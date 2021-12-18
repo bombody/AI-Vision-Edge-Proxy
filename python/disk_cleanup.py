5f1f5c17347bbed63571ce1a7117d28982ce6a7f
bombody <135224248+bombody@users.noreply.github.com>
2021-12-18 07:20:57 +0000

    Create .htaccess

--- python/disk_cleanup.py
+++ python/disk_cleanup.py
@@ -29,4 +29,12 @@ class CleanupScheduler(threading.Thread):
         self.__scheduler = sched.scheduler(time.time, time.sleep)
         self.__units = {'s':'seconds', 'm':'minutes', 'h':'hours', 'd':'days', 'w':'weeks'}
 
-        self
+        self.__delay_seconds = self.convert_to_seconds(remove_older_than)
+
+    def convert_to_seconds(self, s):
+        return int(timedelta(**{
+            self.__units.get(m.group('unit').lower(), 'seconds'): int(m.group('val'))
+            for m in re.finditer(r'(?P<val>\d+)(?P<unit>[smhdw]?)', s, flags=re.I)
+        }).total_seconds())
+
+    def remove_mp4_files(self):
