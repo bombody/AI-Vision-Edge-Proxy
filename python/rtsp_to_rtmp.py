fc174e0bce8285d320061bbf12c58d4d32b93c9d
bombody <135224248+bombody@users.noreply.github.com>
2022-02-25 23:03:51 +0000

    Clarify documentation

--- python/rtsp_to_rtmp.py
+++ python/rtsp_to_rtmp.py
@@ -7,4 +7,18 @@
 #    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS I
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import av
+import time
+import os
+import io
+from av.filter import Filter, Graph
+from av.codec import CodecContext
+import redis
+import threading, queue
+from read_image import ReadImage
+import
