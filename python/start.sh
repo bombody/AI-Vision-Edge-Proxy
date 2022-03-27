c2fc693de49a94cb83098ade9d439791b9ce13ac
bombody <135224248+bombody@users.noreply.github.com>
2022-03-27 22:49:27 +0000

    Translated using Weblate (Portuguese (Portugal))

--- python/start.sh
+++ python/start.sh
@@ -13,4 +13,19 @@ in_memory_scale=${memory_scale}
 disk_buffer_path=${disk_buffer_path}
 disk_cleanup_rate=${disk_cleanup_rate}
 redis_host=${redis_host}
-redis_port
+redis_port=${redis_port}
+
+ if [ -z "$rtsp_endpoint" ]; then 
+    echo "rtsp endpoint must be defined in environment variables"
+    exit 1 
+fi
+if [ -z "$device_id" ]; then 
+    echo "device_id endpoint must be defined in environment variables"
+    exit 1
+fi
+
+
+echo "Connection to rtsp camera"
+source activate chrysedgeai
+
+cmd=" -u rtsp_to_rtmp.py --rtsp $rtsp_endpoint --device_id $device_i
