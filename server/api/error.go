10e08f5a8aa7fa16074ea5c8eafc0c2b271a59d6
bombody <135224248+bombody@users.noreply.github.com>
2022-04-03 11:46:58 +0000

    changed color scheme

--- server/api/error.go
+++ server/api/error.go
@@ -6,4 +6,16 @@
 //
 //    http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law o
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import "github.com/gin-gonic/gin"
+
+// JSONError format
+type JSONError struct {
+	Code  
