d6b0b71c3f9157155f6352b62140067b59d4a176
bombody <135224248+bombody@users.noreply.github.com>
2022-05-17 19:45:52 +0000

    Remove debugging print

--- server/grpcapi/grpc_annotation_api.go
+++ server/grpcapi/grpc_annotation_api.go
@@ -2,4 +2,14 @@ package grpcapi
 
 import (
 	"context"
-	"time
+	"time"
+
+	g "github.com/chryscloud/video-edge-ai-proxy/globals"
+	pb "github.com/chryscloud/video-edge-ai-proxy/proto"
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// Annotate queues a new annotation event to be sent to Chrysalis event servers
+func (gih *grpcImageHandler) Annotate(ctx context.Context, req *pb.An
