5246167b1e5d4c95f07810129017685bd10d8e28
bombody <135224248+bombody@users.noreply.github.com>
2022-05-18 12:07:50 +0000

    fix phpcs

--- server/grpcapi/grpc_annotation_api.go
+++ server/grpcapi/grpc_annotation_api.go
@@ -12,4 +12,11 @@ import (
 )
 
 // Annotate queues a new annotation event to be sent to Chrysalis event servers
-func (gih *grpcImageHandler) Annotate(ctx context.Context, req *pb.An
+func (gih *grpcImageHandler) Annotate(ctx context.Context, req *pb.AnnotateRequest) (*pb.AnnotateResponse, error) {
+	if gih.edgeKey == nil {
+		settings, err := gih.settingsManager.Get()
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "failed to read settings")
+		}
+		if settings.EdgeKey == "" {
+			return nil, status.Errorf(codes.InvalidArgument, "Can't find edge key in settings. required to use an
