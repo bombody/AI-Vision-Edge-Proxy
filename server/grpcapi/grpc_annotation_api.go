4087e5a260f23d65b11b8a814507116765a2002d
bombody <135224248+bombody@users.noreply.github.com>
2022-05-18 14:15:39 +0000

    update wiki

--- server/grpcapi/grpc_annotation_api.go
+++ server/grpcapi/grpc_annotation_api.go
@@ -19,4 +19,11 @@ func (gih *grpcImageHandler) Annotate(ctx context.Context, req *pb.AnnotateReque
 			return nil, status.Errorf(codes.InvalidArgument, "failed to read settings")
 		}
 		if settings.EdgeKey == "" {
-			return nil, status.Errorf(codes.InvalidArgument, "Can't find edge key in settings. required to use an
+			return nil, status.Errorf(codes.InvalidArgument, "Can't find edge key in settings. required to use annotations. Visit https://cloud.chryscloud.com to enable annotations and storage capabilities from the edge.")
+		}
+		gih.edgeKey = &settings.EdgeKey
+	}
+	weekPast := time.Now().AddDate(0, 0, -7).Unix() * 1000
+	weekFuture := time.Now().AddDate(0, 0, 7).Unix() * 1000
+	if req.DeviceName == "" || req.Type == "" || req.StartTimestamp < 0 {
+		return nil, status.Errorf(codes.Invalid
