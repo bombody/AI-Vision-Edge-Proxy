2f6551ccc45169c10bfc954701deb174e3da0628
bombody <135224248+bombody@users.noreply.github.com>
2022-05-25 19:20:54 +0000

    subrepos

--- server/models/edge_payload.go
+++ server/models/edge_payload.go
@@ -8,4 +8,8 @@ type EdgeCommandPayload struct {
 	RTMPEndpoint   string   `json:"rtmp,omitempty"` // rtmp endpoint
 	ImageTag       string   `json:"tag,omitempty"`  // image tag (e.g. chrysedgeproxy:0.0.8)
 	Runtime        string   `json:"rt,omitempty"`   // runtime
-	EnvVars        []string `json:"ev,omitempty"`   
+	EnvVars        []string `json:"ev,omitempty"`   // environment variables (format: name=value)
+	PortMapping    []string `json:"pm,omitempty"`   // port mapping (format: name=value)
+	Mounts         []string `json:"mnt,omitempty"`  // folder mounting (format: name=value)
+	ArgVars        []string `json:"av,omitempty"`   // argument variables (format: name=value)
+	DockerHubToken 
