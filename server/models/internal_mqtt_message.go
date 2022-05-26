263b165a0977c344d4770951635b0614aac7e48f
bombody <135224248+bombody@users.noreply.github.com>
2022-05-26 14:00:43 +0000

    Work on completion events: t+

--- server/models/internal_mqtt_message.go
+++ server/models/internal_mqtt_message.go
@@ -6,4 +6,9 @@ type MQTTProcessType string
 const (
 	DeviceOperationAdd      string = "add"           // add device
 	DeviceOperationRemove   string = "remove"        // remove device
-	DeviceOperationState    string = "state"   
+	DeviceOperationState    string = "state"         // the current state of device
+	DeviceOperationStats    string = "stats"         // device stats (host system and each device)
+	GatewayOperationCheckIn string = "gwcheckin"     // gateway checkin
+	DeviceInternalTesting   string = "internal_test" // internal development event. Not used or required for regular operations (TODO: movw to unit tests)
+
+	DeviceOper
