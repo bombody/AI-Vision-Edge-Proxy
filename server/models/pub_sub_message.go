3d3ba07d9003da05a3df9f7398a3677e71eeae4e
bombody <135224248+bombody@users.noreply.github.com>
2022-05-28 13:15:42 +0000

    cpufreq: interactive: add timer slack to limit idle at speed > min

--- server/models/pub_sub_message.go
+++ server/models/pub_sub_message.go
@@ -5,4 +5,5 @@ type PubSubMessage struct {
 	DeviceID      string `json:"deviceId"`
 	FromTimestamp int64  `json:"fromTimestamp"`
 	ToTimestamp   int64  `json:"toTimestamp"`
-	RequestID     str
+	RequestID     string `json:"requestId"`
+}
