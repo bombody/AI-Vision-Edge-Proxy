a2d68a3e6e631c17c39492539c60b575b6cd44d0
bombody <135224248+bombody@users.noreply.github.com>
2022-07-05 10:06:45 +0000

    Tweak the README

--- server/mqtt/mqtt_process_operations.go
+++ server/mqtt/mqtt_process_operations.go
@@ -4,4 +4,15 @@ import (
 	"encoding/json"
 	"errors"
 	"strconv"
-	"
+	"strings"
+	"time"
+
+	"github.com/chryscloud/go-microkit-plugins/docker"
+	g "github.com/chryscloud/video-edge-ai-proxy/globals"
+	"github.com/chryscloud/video-edge-ai-proxy/models"
+	"github.com/chryscloud/video-edge-ai-proxy/utils"
+)
+
+// Removes a camera from the edge
+func (mqtt *mqttManager) StopCamera(configPayload []byte) error {
+	g.Log.Info("received
