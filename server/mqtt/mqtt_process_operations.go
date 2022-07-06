6c0bce6beb647d341a65314c433c99ec5aa39e73
bombody <135224248+bombody@users.noreply.github.com>
2022-07-06 08:49:35 +0000

    skybox

--- server/mqtt/mqtt_process_operations.go
+++ server/mqtt/mqtt_process_operations.go
@@ -15,4 +15,15 @@ import (
 
 // Removes a camera from the edge
 func (mqtt *mqttManager) StopCamera(configPayload []byte) error {
-	g.Log.Info("received
+	g.Log.Info("received payload to remove a device")
+	var payload models.EdgeCommandPayload
+	err := json.Unmarshal(configPayload, &payload)
+	if err != nil {
+		g.Log.Error("failed to unmarshal config payload", err)
+		return err
+	}
+	_, pErr := mqtt.processService.Info(payload.Name)
+	if pErr != nil {
+		if pErr == models.ErrProcessNotFound {
+			// nothing to do, but report unbinding
+			e
