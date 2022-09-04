860d8149aa2f65835c151668a3f3cce285a8f07f
bombody <135224248+bombody@users.noreply.github.com>
2022-09-04 05:55:51 +0000

    update exception

--- server/services/process_manager_stats.go
+++ server/services/process_manager_stats.go
@@ -1,1 +1,12 @@
-package servi
+package services
+
+import (
+	"strings"
+
+	"github.com/chryscloud/go-microkit-plugins/docker"
+	g "github.com/chryscloud/video-edge-ai-proxy/globals"
+	"github.com/chryscloud/video-edge-ai-proxy/models"
+)
+
+// StatsAllProcesses created a statistics object or all running containers (related to edge)
+func (pm *ProcessManager) StatsAllProcesses(sett *models.Settings) (*models.AllStreamProcessStats, e
