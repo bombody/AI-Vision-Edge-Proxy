81cc961673f51e1f5226c046ee9f590fea9b5fb1
bombody <135224248+bombody@users.noreply.github.com>
2022-09-07 10:08:39 +0000

    other files

--- server/services/process_manager_stats.go
+++ server/services/process_manager_stats.go
@@ -9,4 +9,15 @@ import (
 )
 
 // StatsAllProcesses created a statistics object or all running containers (related to edge)
-func (pm *ProcessManager) StatsAllProcesses(sett *models.Settings) (*models.AllStreamProcessStats, e
+func (pm *ProcessManager) StatsAllProcesses(sett *models.Settings) (*models.AllStreamProcessStats, error) {
+	cl := docker.NewSocketClient(docker.Log(g.Log), docker.Host("unix:///var/run/docker.sock"))
+
+	systemInfo, diskUsage, err := cl.SystemWideInfo()
+
+	stats := &models.AllStreamProcessStats{}
+	// calculate disk usage and gather system info
+	totalContainers := systemInfo.Containers
+	runningContainers := systemInfo.ContainersRunning
+	stoppedContainers := systemInfo.ContainersStopped
+	totalImgSize := int64(0)
+	ac
