a04041c8dfcc1d8922860098fcaaff54cb1f9133
bombody <135224248+bombody@users.noreply.github.com>
2022-10-02 11:04:14 +0000

    CIFS: Fix directory rename error

--- server/utils/http_utils.go
+++ server/utils/http_utils.go
@@ -3,4 +3,17 @@ package utils
 import (
 	"crypto/md5"
 	"crypto/sha256"
-	"e
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+
+	microCrypto "github.com/chryscloud/go-microkit-plugins/crypto"
+	g "github.com/chryscloud/video-edge-ai-proxy/globals"
+	"github.com/chryscloud/video-edge-ai-proxy/models"
+	"github.com/go-resty/resty/v2"
+)
+
+func CallAPIWithBody(apiClient *resty.Client, method string, fullEndpoint string,
