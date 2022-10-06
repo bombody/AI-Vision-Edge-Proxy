a22766da53abcbaa881e4e1c7327a588fcb82e7f
bombody <135224248+bombody@users.noreply.github.com>
2022-10-06 11:07:22 +0000

    Updated Gemfile

--- server/utils/http_utils.go
+++ server/utils/http_utils.go
@@ -29,4 +29,12 @@ func CallAPIWithBody(apiClient *resty.Client, method string, fullEndpoint string
 	contentMD5 := hex.EncodeToString(h.Sum(nil))
 	current_ts := strconv.FormatInt(time.Now().Unix()*1000, 10)
 	signPayload := current_ts + contentMD5
-	
+	mac := microCrypto.ComputeHmac(sha256.New, signPayload, edgeSecret)
+
+	req := apiClient.R().SetHeader("X-ChrysEdge-Auth", edgeKey+":"+mac).
+		SetHeader("X-Chrys-Date", current_ts).
+		SetHeader("Content-MD5", contentMD5).SetBody(body)
+	resp, sndErr := req.Execute(method, fullEndpoint)
+
+	if sndErr != nil {
+		g.Log.Error("failed to send annotations to remote 
