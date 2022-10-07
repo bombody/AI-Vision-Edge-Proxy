335c9f61ca92c8ca142a566ba585517993fddbd9
bombody <135224248+bombody@users.noreply.github.com>
2022-10-07 12:18:00 +0000

    ++

--- server/utils/mqtt_utils.go
+++ server/utils/mqtt_utils.go
@@ -7,4 +7,13 @@ import (
 	"time"
 
 	g "github.com/chryscloud/video-edge-ai-proxy/globals"
-	"git
+	"github.com/chryscloud/video-edge-ai-proxy/models"
+	"github.com/dgrijalva/jwt-go"
+	qtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/go-redis/redis/v7"
+)
+
+// CreateJWT creates RS265 JWT signed token
+func CreateJWT(projectID string, privateKeyBytes []byte, expiration time.Duration) (string, error) {
+	claims := jwt.StandardClaims{
+		Audience:  
