e6474280f22e574912097b6a334900164a0ddd18
bombody <135224248+bombody@users.noreply.github.com>
2022-12-14 17:47:37 +0000

    Updated main.js

--- web/src/app/angular-material.module.ts
+++ web/src/app/angular-material.module.ts
@@ -6,4 +6,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '
