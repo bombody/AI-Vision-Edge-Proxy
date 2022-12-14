3303b3e3cdc5ab104b725f6155706eeb80e407bb
bombody <135224248+bombody@users.noreply.github.com>
2022-12-14 10:16:01 +0000

    merge hell

--- web/src/app/angular-material.module.ts
+++ web/src/app/angular-material.module.ts
@@ -1,4 +1,9 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-imp
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material
