9b550c87b78696a3c7a6e5fd090ce0f8e4cbcc80
bombody <135224248+bombody@users.noreply.github.com>
2023-01-01 17:26:34 +0000

    Fix documentation errors

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,2 +1,6 @@
 import { NgModule } from '@angular/core';
-imp
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProcessesComponent } from './components/processes/processes.component';
+import { ProcessDetailsComponent } from './components/process-details/process-details.component';
+import { ProcessAddComponent } from './components/process-add/process-add.c
