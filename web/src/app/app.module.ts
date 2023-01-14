a201ecdc59f4829699bb1413802f4272c82b585d
bombody <135224248+bombody@users.noreply.github.com>
2023-01-14 16:02:17 +0000

    testing fixes

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -7,4 +7,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CookieService } from 'ngx-cookie-service';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularMaterialModule } from './angular-material.module';
+import { LoaderComponent, LoaderService } from './components/loader/loader.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ChrysHttpInterceptor } from './interceptors/http.interceptor';
+import { ProcessesComponent } from './c
