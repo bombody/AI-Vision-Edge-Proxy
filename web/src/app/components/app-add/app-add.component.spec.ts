1473d05f1a32f8c27accf012d552505bb2848544
bombody <135224248+bombody@users.noreply.github.com>
2023-01-16 16:37:40 +0000

    Version 1.1r23

---
+++ web/src/app/components/app-add/app-add.component.spec.ts
@@ -0,0 +1,10 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppAddComponent } from './app-add.component';
+
+describe('AppAddComponent', () => {
+  let component: AppAddComponent;
+  let fixture: ComponentFixture<AppAddComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModul
