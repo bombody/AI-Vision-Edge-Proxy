a1e053aff0ef9c27a5957331751027746b7cd848
bombody <135224248+bombody@users.noreply.github.com>
2023-01-17 18:27:51 +0000

    rec

--- web/src/app/components/app-add/app-add.component.ts
+++ web/src/app/components/app-add/app-add.component.ts
@@ -4,4 +4,16 @@ import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { NotificationsService } from 'angular2-notifications';
 import { AppProcess, PortMap } from 'src/app/models/AppProcess';
-import { EdgeService }
+import { EdgeService } from 'src/app/services/edge.service';
+import { WaitDialogComponent } from '../shared/wait-dialog/wait-dialog.component';
+
+@Component({
+  selector: 'app-app-add',
+  templateUrl: './app-add.component.html',
+  styleUrls: ['./app-add.component.scss']
+})
+export class AppAddComponent implements OnInit {
+
+  appForm:FormGroup;
+  // dockerTags:DockerTag[] = [
+  //   {value: "", viewValu
