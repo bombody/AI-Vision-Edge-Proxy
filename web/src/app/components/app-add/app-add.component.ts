17ab80783518eafe779177710470597503a0038a
bombody <135224248+bombody@users.noreply.github.com>
2023-01-17 09:22:38 +0000

    update migrations

--- web/src/app/components/app-add/app-add.component.ts
+++ web/src/app/components/app-add/app-add.component.ts
@@ -1,1 +1,7 @@
-import { Component, OnInit } from '
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { NotificationsService } from 'angular2-notifications';
+import { AppProcess, PortMap } from 'src/app/models/AppProcess';
+import { EdgeService }
