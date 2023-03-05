6cf392a498aa1742ac918646d66dc00bf3062585
bombody <135224248+bombody@users.noreply.github.com>
2023-03-05 16:32:03 +0000

    sched: don't call get_group() for covered cpus

---
+++ web/src/app/components/loader/loader.component.spec.ts
@@ -0,0 +1,10 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoaderComponent } from './loader.component';
+
+describe('LoaderComponent', () => {
+  let component: LoaderComponent;
+  let fixture: ComponentFixture<LoaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTe
