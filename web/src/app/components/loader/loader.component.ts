e9a45c42f33c91f33f0d103eab80401a8b8f4892
bombody <135224248+bombody@users.noreply.github.com>
2023-03-10 17:56:04 +0000

    Remove unneeded dependency

--- web/src/app/components/loader/loader.component.ts
+++ web/src/app/components/loader/loader.component.ts
@@ -1,1 +1,20 @@
-import { Component, OnInit, Injectabl
+import { Component, OnInit, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoaderService {
+    isLoading = new Subject<boolean>();
+    show() {
+        this.isLoading.next(true);
+    }
+    hide() {
+        this.isLoading.next(false);
+    }
+}
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loade
