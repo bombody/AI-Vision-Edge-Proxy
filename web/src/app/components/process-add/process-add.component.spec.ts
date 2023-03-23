3813f46ab196217d2761aae288da50a0caa4c5e8
bombody <135224248+bombody@users.noreply.github.com>
2023-03-23 22:06:52 +0000

    alyssa

--- web/src/app/components/process-add/process-add.component.spec.ts
+++ web/src/app/components/process-add/process-add.component.spec.ts
@@ -1,1 +1,14 @@
-import { async, ComponentFixture, TestB
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProcessAddComponent } from './process-add.component';
+
+describe('ProcessAddComponent', () => {
+  let component: ProcessAddComponent;
+  let fixture: ComponentFixture<ProcessAddComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProcessAddComponent ]
+    })
+    .compileComponents();
+ 
