a361eb677337b4220202d91cc9c747c2a81687ca
bombody <135224248+bombody@users.noreply.github.com>
2023-03-26 15:32:59 +0000

    Update unitsguide.html

--- web/src/app/components/process-add/process-add.component.ts
+++ web/src/app/components/process-add/process-add.component.ts
@@ -1,2 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } fro
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { EdgeService } from 'src/app/services/edge.service';
+import { StreamProcess } from 'src/app/models/StreamProcess';
+import { Router } from '@angular/router';
+import { NotificationsService } from 'angular2-notifications';
+
+interface DockerTag {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-process-add',
+  templateUrl: './process-add.component.html',
+  styleUrls: [
