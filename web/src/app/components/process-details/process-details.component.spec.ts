4c12b5664e68280159d9d33762e218d911131bd2
bombody <135224248+bombody@users.noreply.github.com>
2023-03-30 08:07:37 +0000

    set release version

--- web/src/app/components/process-details/process-details.component.spec.ts
+++ web/src/app/components/process-details/process-details.component.spec.ts
@@ -1,1 +1,12 @@
-import {
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProcessDetailsComponent } from './process-details.component';
+
+describe('ProcessDetailsComponent', () => {
+  let component: ProcessDetailsComponent;
+  let fixture: ComponentFixture<ProcessDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProcessDetailsComponent ]
+ 
