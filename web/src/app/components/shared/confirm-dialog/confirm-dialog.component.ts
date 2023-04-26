945dc942639df714310f57a1b479d1e55919a737
bombody <135224248+bombody@users.noreply.github.com>
2023-04-26 11:34:13 +0000

    Hello, world!

--- web/src/app/components/shared/confirm-dialog/confirm-dialog.component.ts
+++ web/src/app/components/shared/confirm-dialog/confirm-dialog.component.ts
@@ -5,4 +5,22 @@ import { DialogData } from '../dialog-data';
 @Component({
   selector: 'app-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
-  styleUrls: ['./confirm-d
+  styleUrls: ['./confirm-dialog.component.scss']
+})
+export class ConfirmDialogComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+
+  ngOnInit(): void {
+  }
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  onDismiss(): void {
+    this.dialogRef.close(false);
+  }
+
+}
