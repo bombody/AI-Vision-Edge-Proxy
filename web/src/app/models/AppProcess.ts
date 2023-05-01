bb7dd438747b32cf33196c73ff9df9ff8c15985e
bombody <135224248+bombody@users.noreply.github.com>
2023-05-01 16:04:41 +0000

    Added namespaces

--- web/src/app/models/AppProcess.ts
+++ web/src/app/models/AppProcess.ts
@@ -4,4 +4,25 @@ export interface AppProcess {
     name?:string
     docker_user?:string,
     docker_repository?:string,
-    docker_ve
+    docker_version?:string,
+    env_vars?:VarPair[],
+    mount?:VarPair[],
+    port_mappings?:PortMap[],
+    container_id?:string
+    status?:string
+    state?:State
+    logs?:Logs
+    created?:Number
+    modified?:Number
+    upgrade_available?:boolean
+    newer_version?:string
+    upgrading_now?:boolean
+}
+
+export interface VarPair {
+    name:string,
+    value:string
+}
+
+export interface PortMap {
+    expose
