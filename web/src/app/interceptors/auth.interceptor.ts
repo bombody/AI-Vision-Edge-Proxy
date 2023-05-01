cdeb08420487b41ce61c8bf00e7368bdd4b29eae
bombody <135224248+bombody@users.noreply.github.com>
2023-05-01 08:02:34 +0000

    Minor performance improvements.

--- web/src/app/interceptors/auth.interceptor.ts
+++ web/src/app/interceptors/auth.interceptor.ts
@@ -1,2 +1,13 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/ro
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({ providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+
+    constructor(private cookie:CookieService, private router:Router) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        let ourcookie = this.cookie.get("chrys-token");
+        
+    
